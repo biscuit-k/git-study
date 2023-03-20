f194d12de13d16297b6fd9f85f9e0f207bb892e9
Bisucit <biscuitv@163.com>
2023-03-20 17:15:30 +0800

    拉取中央仓库中最新代码后并修改一定的修改后的本地commit

--- git-a/src/main/java/com/biscuit/a/Test_A.java
+++ git-a/src/main/java/com/biscuit/a/Test_A.java
@@ -8,5 +8,6 @@ public class Test_A {
     public static void main(String[] args) {
         System.out.println("master-branch-init-git-project");
         System.out.println("github-html-update-body-idea-pull-test");
+        System.out.println("master-branch-update");
     }
 }
