58f5c49854c06f07bdd3fea7f29c8484ad593bd8
Bisucit <biscuitv@163.com>
2023-03-20 17:26:39 +0800

    hot-fix branch commit

---
+++ git-a/src/main/java/com/biscuit/b/Test_B.java
@@ -0,0 +1,11 @@
+package com.biscuit.b;
+
+/**
+ * @author biscuit
+ * @create 2023年03月20日 17:23
+ */
+public class Test_B {
+    public static void main(String[] args) {
+        System.out.println("hot-fix branch create class");
+    }
+}
