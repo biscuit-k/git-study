5f6e1774bb980498ca42485a78fcf833732d03d6
Bisucit <biscuitv@163.com>
2023-03-20 16:56:42 +0800

    Master branch initial commit

---
+++ git-a/src/main/java/com/biscuit/a/Test_A.java
@@ -0,0 +1,11 @@
+package com.biscuit.a;
+
+/**
+ * @author biscuit
+ * @create 2023年03月20日 16:54
+ */
+public class Test_A {
+    public static void main(String[] args) {
+        System.out.println("master-branch-init-git-project");
+    }
+}
